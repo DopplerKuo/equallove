1085ccfc443c2f0f382523863ece5731a772a09a
Adele Yu <fhead17@gmail.com>
2016-11-28 01:45:05 +0800

    add pop up box and some content

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,9 +7,18 @@ $(window).load(function() {
 
 
 	/// 婚姻平權進度條
-	
-})
-
+	$(".btn").click(function() {
+		$("body").css("overflow", "hidden");
+		var index = $(this).attr("data-pop");
+		$(".pop-up").fadeIn(300);
+		$(".pop-up-box").fadeOut(0);
+		$(".pop-up-box[data-show=" + index + "]").fadeIn(0);
+		$(".close-circle").click(function() {
+			$(".pop-up").fadeOut(0);
+			$("body").css("overflow", "auto");
+		});
+	});
+});
 
 
 
