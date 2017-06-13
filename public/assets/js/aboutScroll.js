3bcd8b67831bd2936ffec4b27cb2a5ed0d01c664
DopplerKuo <doppler.kuo@gmail.com>
2017-06-14 04:34:16 +0800

    首頁

--- public/assets/js/aboutScroll.js
+++ public/assets/js/aboutScroll.js
@@ -1,18 +1,17 @@
 $(function(){
-	$(window).bind('scroll resize', function(){
-　　var $this = $(this);
-　　var $this_Top = $this.scrollTop();
-   var $barFixedHeight = $('.sec1').height() - 140;
-   var a = $this_Top / 4
-   var o = 1 - (1 / a)
-　　if($this_Top < $barFixedHeight){
-				$('.sec1 .content').css("transform", "translate3d(0," + a + "px,0)")
-　　　}
-　　}).scroll();
-	$(".go-down-btn").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $("#scrolltohere").offset().top
-	    }, 800);
-	});
+	
+	$(window).bind('resize', function() {
+		$nav = $('#nav').offset().top
+	}).resize();
+
+	$(window).bind('scroll resize', function() {
+		let $this_Top = $(this).scrollTop();
+		if ( $this_Top > $nav ) {
+			$('body').addClass('nav-fixed')
+		} else {
+			$('body').removeClass('nav-fixed')
+		}
+　}).scroll();
+	
 })
 
